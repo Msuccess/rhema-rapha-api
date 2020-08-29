befc944b3670486ebeb091de5ca673fc7c1f7220
Moses Nkrumah <mosessccss@gmail.com>
2020-08-29 01:39:22 +0000

    .

--- ormconfig.js
+++ ormconfig.js
@@ -1,5 +1,4 @@
 module.exports = [{
-
   "name": "default",
   "type": "postgres",
   "url": "postgres://affckevcfcmbcf:cdb4e4600acd1e7519006ab72267cdcac86e18062d2008c7223d21800d7d4818@ec2-34-236-215-156.compute-1.amazonaws.com:5432/d6hqlj8st87sfu",
@@ -11,7 +10,7 @@ module.exports = [{
   "password": "cdb4e4600acd1e7519006ab72267cdcac86e18062d2008c7223d21800d7d4818",
   "database": "d6hqlj8st87sfu",
   "entities": ["dist/**/*.entity{.ts,.js}"],
-  "synchronize": true
+  "synchronize": true,
 
 }, {
   "name": "dev",

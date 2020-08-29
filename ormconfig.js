ac357d467b0025cdeed223c930ad5f19bcc6dbca
Moses Nkrumah <mosessccss@gmail.com>
2020-08-29 01:35:08 +0000

    change host

--- ormconfig.js
+++ ormconfig.js
@@ -3,7 +3,7 @@ module.exports = [{
   "name": "default",
   "type": "postgres",
   "url": "postgres://affckevcfcmbcf:cdb4e4600acd1e7519006ab72267cdcac86e18062d2008c7223d21800d7d4818@ec2-34-236-215-156.compute-1.amazonaws.com:5432/d6hqlj8st87sfu",
-  "host": "localhost",
+  "host": "ec2-34-236-215-156.compute-1.amazonaws.com",
   "port": 5432,
   "keepConnectionAlive": true,
   "autoLoadEntities": true,

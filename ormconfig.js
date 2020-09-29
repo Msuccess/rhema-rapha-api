f3506216950a975ba3d6cd110290714c965f208c
Moses Nkrumah <mosessccss@gmail.com>
2020-09-29 23:34:29 +0000

    git

--- ormconfig.js
+++ ormconfig.js
@@ -15,17 +15,4 @@ module.exports = [
     entities: ['dist/**/*.entity{.ts,.js}'],
     synchronize: true,
   },
-  {
-    name: 'dev',
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    keepConnectionAlive: true,
-    autoLoadEntities: true,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'RhemaRaphaDb',
-    entities: ['dist/**/*.entity{.ts,.js}'],
-    synchronize: true,
-  },
 ];

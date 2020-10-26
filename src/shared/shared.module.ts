1e379ee1343c51d4748656a0f6803a84709347fb
Moses Nkrumah <mosessccss@gmail.com>
2020-10-26 02:28:05 +0000

    remove notification class

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,15 +1,13 @@
-import { NotificationController } from './service/notification.controller';
 import { IdentityUserService } from './../authentication/identity-user/identity-user.service';
 import { Module } from '@nestjs/common';
 import { IdentityUserRepository } from '../authentication/identity-user/identity-user.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { NotificationService } from './service/notification.service';
 import { EmailService } from './service/email.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([IdentityUserRepository])],
-  controllers: [NotificationController],
-  providers: [IdentityUserService, NotificationService, EmailService],
+  controllers: [],
+  providers: [IdentityUserService, EmailService],
   exports: [IdentityUserService, EmailService],
 })
 export class SharedModule {}
